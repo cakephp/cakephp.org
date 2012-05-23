6ab9f9b4dd27bd4e60dde20b4bba9e8e11ad2e11
Mark Story <mark@mark-story.com>
2012-05-22 21:57:42 -0400

    Update links for new release.

--- Controller/AppController.php
+++ Controller/AppController.php
@@ -46,11 +46,11 @@ class AppController extends Controller {
 		);
 
 		$three = array(
-			'version' => '2.1.2 ' . __('Stable'),
-			'date' => 'March 26, 2012',
-			'link' => 'https://github.com/cakephp/cakephp/zipball/2.1.2',
-			'announcement' => 'http://bakery.cakephp.org/articles/lorenzo/2012/03/26/cakephp_2_1_1_and_1_3_15_released',
-			'changelog' => 'http://cakephp.org/changelogs/2.1.2'
+			'version' => '2.1.3 ' . __('Stable'),
+			'date' => 'May 22, 2012',
+			'link' => 'https://github.com/cakephp/cakephp/zipball/2.1.3',
+			'announcement' => 'http://bakery.cakephp.org/articles/markstory/2012/05/23/cakephp_2_1_3_2_2_0-rc1_released',
+			'changelog' => 'http://cakephp.org/changelogs/2.1.3'
 		);
 
 		$this->set(compact('one', 'two', 'three'));
