0b2357540983ed2fd3d0b315a3480d4e4909ae0f
Graham Weldon <graham@grahamweldon.com>
2012-11-03 22:55:12 +0900

    Added deployment files

---
+++ fabfile.py
@@ -0,0 +1,29 @@
+from fabric.api import env, run, local, task, settings, sudo
+import json
+from deploy.common import *
+
+env.hosts = ['cakephp@cakephp.org']
+
+environmentfile = openenvfile(__file__)
+environments = json.loads(environmentfile.read())
+
+timestr = ''
+deployconf = {}
+
+@task(default=True)
+def deploy(environment='stage'):
+	global deployconf, timestr
+	timestr = run('date +%Y%m%d.%H%M').strip()
+	envs = init_environments(environments)
+	deployconf = envs[get_environment(environment, envs)]
+
+	clone(deployconf, timestr)
+	delete(deployconf, timestr)
+	config(deployconf, timestr)
+	# links(deployconf, timestr)
+	current_link(deployconf, timestr)
+	asset_generate(deployconf, timestr)
+	services(deployconf, timestr)
+
+def asset_generate(deployconf, timestr):
+	run('{0}lib/Cake/Console/cake -app {0}{1} AssetCompress.asset_compress build'.format(deployconf['site_dir'], deployconf['current_dir']))
