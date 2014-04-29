7ed28bfbeaab83e51509cbc2d2184d57fffa660c
Mark Story <mark@mark-story.com>
2014-04-28 20:52:36 -0400

    Bump version number for 2.4.8

--- Controller/AppController.php
+++ Controller/AppController.php
@@ -38,19 +38,19 @@ class AppController extends Controller {
 		);
 		
 		$two = array(
-			'version' => '1.3.16 ' . __('Stable'), 
-			'date' => 'April 28, 2013',
-			'link' => 'https://github.com/cakephp/cakephp/zipball/1.3.16',
-			'announcement' => 'http://bakery.cakephp.org/articles/markstory/2013/04/28/security_release_-_cakephp_1_2_12_1_3_16_2_2_8_and_2_3_4',
-			'changelog' => 'http://cakephp.org/changelogs/1.3.16'
+			'version' => '1.3.18 ' . __('Stable'), 
+			'date' => 'April 28, 2014',
+			'link' => 'https://github.com/cakephp/cakephp/zipball/1.3.18',
+			'announcement' => 'http://bakery.cakephp.org/articles/markstory/http://bakery.cakephp.org/articles/markstory/2014/04/29/cakephp_1_3_18_and_2_4_8_released',
+			'changelog' => 'http://cakephp.org/changelogs/1.3.18'
 		);
 
 		$three = array(
 			'version' => '2.4.7 ' . __('Stable'),
 			'date' => 'April 4, 2014',
-			'link' => 'https://github.com/cakephp/cakephp/zipball/2.4.7',
-			'announcement' => 'http://bakery.cakephp.org/articles/markstory/2014/04/04/cakephp_2_4_7_released',
-			'changelog' => 'http://cakephp.org/changelogs/2.4.7'
+			'link' => 'https://github.com/cakephp/cakephp/zipball/2.4.8',
+			'announcement' => 'http://bakery.cakephp.org/articles/markstory/2014/04/29/cakephp_1_3_18_and_2_4_8_released',
+			'changelog' => 'http://cakephp.org/changelogs/2.4.8'
 		);
 
 		$this->set(compact('one', 'two', 'three'));
