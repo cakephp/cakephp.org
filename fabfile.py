4c2e7c83aa51222646190f0317e5f5ae6a4217a2
Alejandro Ibarra <alejandro.ibarra@cakedc.com>
2015-03-20 17:35:48 -0500

    Adding environment 3.0 to fab file.

--- fabfile.py
+++ fabfile.py
@@ -11,20 +11,21 @@ timestr = ''
 deployconf = {}
 
 @task(default=True)
-def deploy(environment='stage'):
+def deploy(environment='3.0'):
 	global deployconf, timestr
 	timestr = run('date +%Y%m%d.%H%M').strip()
 	envs = init_environments(environments)
 	deployconf = envs[get_environment(environment, envs)]
-
 	clone(deployconf, timestr)
 	delete(deployconf, timestr)
-	config(deployconf, timestr)
-	# links(deployconf, timestr)
 	fullwritable(deployconf, timestr)
 	current_link(deployconf, timestr)
+	composer(deployconf, timestr)
 	asset_generate(deployconf, timestr)
-	services(deployconf, timestr)
 
 def asset_generate(deployconf, timestr):
-	run('{0}lib/Cake/Console/cake -app {0}{1} AssetCompress.asset_compress build'.format(deployconf['site_dir'], deployconf['current_dir']))
+	run('{0}{1}/bin/cake AssetCompress.asset_compress build'.format(deployconf['site_dir'], deployconf['current_dir']))
+
+def composer(deployconf, timestr):
+        run('cd {0}{1} && (curl -s https://getcomposer.org/installer | php && php composer.phar install --prefer-dist --optimize-autoloader --ignore-platform-reqs)'.format(deployconf['site_dir'], deployconf['current_dir']))
+
