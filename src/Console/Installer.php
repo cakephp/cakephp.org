79f5b20c858db7af364201d78ff90fa9c2e4a458
Jose Diaz-Gonzalez <myaccounts@savant.be>
2015-03-22 14:39:42 -0400

    Always set perms
    This is necessary as deploys will otherwise stall and we won't have css. Boo.

--- src/Console/Installer.php
+++ src/Console/Installer.php
@@ -39,27 +39,7 @@ class Installer
 
         static::createAppConfig($rootDir, $io);
 
-        // ask if the permissions should be changed
-        if ($io->isInteractive()) {
-            $validator = (function ($arg) {
-                if (in_array($arg, ['Y', 'y', 'N', 'n'])) {
-                    return $arg;
-                }
-                throw new Exception('This is not a valid answer. Please choose Y or n.');
-            });
-            $setFolderPermissions = $io->askAndValidate(
-                '<info>Set Folder Permissions ? (Default to Y)</info> [<comment>Y,n</comment>]? ',
-                $validator,
-                false,
-                'Y'
-            );
-
-            if (in_array($setFolderPermissions, ['Y', 'y'])) {
-                static::setFolderPermissions($rootDir, $io);
-            }
-        } else {
-            static::setFolderPermissions($rootDir, $io);
-        }
+        static::setFolderPermissions($rootDir, $io);
 
         static::setSecuritySalt($rootDir, $io);
 
