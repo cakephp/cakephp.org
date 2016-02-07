8aa50ed5ff9bf1b63de568d071dde26d3385d37b
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2016-02-06 20:00:40 -0500

    Ensure we call the parent beforeRender() method

--- src/Controller/ErrorController.php
+++ src/Controller/ErrorController.php
@@ -2,6 +2,8 @@
 namespace App\Controller;
 
 use Cake\Controller\ErrorController as CoreErrorController;
+use Cake\Core\Configure;
+use Cake\Event\Event;
 
 class ErrorController extends CoreErrorController
 {
@@ -10,4 +12,20 @@ class ErrorController extends CoreErrorController
         'Form',
         'Html',
     ];
+
+    /**
+     * Called after the controller action is run, but before the view is rendered. You can use this method
+     * to perform logic or set view variables that are required on every request.
+     *
+     * @param Event $event An Event instance
+     * @return void
+     * @link http://book.cakephp.org/3.0/en/controllers.html#request-life-cycle-callbacks
+     */
+    public function beforeRender(Event $event)
+    {
+        $this->set([
+            '_version' => Configure::read('App.version'),
+        ]);
+        parent::beforeRender($event);
+    }
 }
