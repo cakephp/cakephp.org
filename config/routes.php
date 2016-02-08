99d1bc119341002d9a7b0df4896020de5464aad1
Larry E. Masters <phpnut@cakephp.org>
2016-02-08 05:13:47 -0600

    Adding route for logos back. This is needed for old links to the page

--- config/routes.php
+++ config/routes.php
@@ -64,6 +64,7 @@ Router::scope('/', function ($routes) {
     $routes->connect('/documentation', ['controller' => 'Pages', 'action' => 'display', 'documentation']);
     $routes->connect('/privacy', ['controller' => 'Pages', 'action' => 'display', 'privacy']);
     $routes->connect('/assets', ['controller' => 'Pages', 'action' => 'display', 'assets']);
+    $routes->connect('/logos', ['controller' => 'Pages', 'action' => 'display', 'trademark']);
     $routes->connect('/trademark', ['controller' => 'Pages', 'action' => 'display', 'trademark']);
 
     $servicesUrl = ['controller' => 'Pages', 'action' => 'display', 'services'];
