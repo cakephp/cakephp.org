4aef7307df685f56e3e25fedc58bf996a32cb87a
Pedro Fernandes Steimbruch <pedro.steimbruch@cakedc.com>
2016-03-26 16:57:13 -0300

    Refs #17122 adding Burzum file storage and first configuration

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -181,7 +181,9 @@ Request::addDetector('tablet', function ($request) {
  */
 
 Plugin::load('Migrations');
-Plugin::load('Showcase', ['routes' => true]);
+Plugin::load('Burzum/FileStorage');
+include('file_storage.php');
+Plugin::load('Showcase', ['routes' => true, 'bootstrap' => true]);
 
 // Only try to load DebugKit in development mode
 // Debug Kit should not be installed on a production system
---
+++ config/file_storage.php
@@ -0,0 +1,15 @@
+<?php
+use Cake\Event\EventManager;
+use Burzum\FileStorage\Lib\FileStorageUtils;
+use Burzum\FileStorage\Lib\StorageManager;
+use Burzum\FileStorage\Event\ImageProcessingListener;
+use Burzum\FileStorage\Event\LocalFileStorageListener;
+
+$listener = new LocalFileStorageListener();
+EventManager::instance()->on($listener);
+
+StorageManager::config('Local', [
+    'adapterOptions' => [ROOT . DS . 'files', true],
+    'adapterClass' => '\Gaufrette\Adapter\Local',
+    'class' => '\Gaufrette\Filesystem'
+]);
