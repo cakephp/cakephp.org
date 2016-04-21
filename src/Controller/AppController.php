d17048da305404a211a189a1f8a2ea4daf058b4e
Jose Alejandro Alvarez Mendoza <jose.mendoza@cakedc.com>
2016-04-21 12:10:33 -0430

    Adding beforeFilter to set meta content

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -66,4 +66,30 @@ class AppController extends Controller
 			]);
     }
 
+	public function beforeFilter(Event $event)
+	{
+		parent::beforeFilter($event);
+		$pages = ['/', '/documentation/videos', '/pages/privacy', '/pages/trademark', '/showcase', '/pages/newsletter'];
+
+		if ($this->request->here === '/') {
+			$this->set('metaOptions', ['title' => 'CakePHP Homepage | Rapid Development PHP Framework', 'description' => 'CakePHP is an open-source web, rapid development framework that makes building web applications simpler, faster and require less code. It follows the model–view–controller (MVC) . Manual for beginners now available and links towards the last version.']);
+		}
+		if ($this->request->here === '/documentation/videos') {
+			$this->set('metaOptions', ['title' => 'CakePHP | Learn CakePHP from the experts | Video Training', 'description' => 'CakePHP making building web applications simpler, faster and require less code. Manual, training and guides for beginners now available and links towards the last version.']);
+		}
+		if ($this->request->here === '/pages/privacy') {
+			$this->set('metaOptions', ['title' => 'CakePHP Rapid Development PHP Framework | Privacy policy', 'description' => 'CakePHP Privacy Policy.']);
+		}
+		if ($this->request->here === '/pages/trademark') {
+			$this->set('metaOptions', ['title' => 'CakePHP Rapid Development PHP Framework | Logos and trademarks', 'description' => 'CakePHP Logos and Trademark policies.']);
+		}
+		if ($this->request->here === '/showcase') {
+			$this->set('metaOptions', ['title' => 'CakePHP Rapid Development PHP Framework | Success story showcases', 'description' => 'CakePHP making building web applications simpler, faster and require less code. Success stores - how CakePHP has helped other companies to succeed.']);
+		}
+		if ($this->request->here === '/pages/newsletter') {
+			$this->set('metaOptions', ['title' => 'CakePHP Rapid Development PHP Framework | CakePHP latest news', 'description' => 'Latest CakePHP News.']);
+		}
+
+		$this->set('changeTitle', in_array($this->request->here, $pages));
+	}
 }
