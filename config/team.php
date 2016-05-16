73ebffb96143faa2e07fa100dbfa873629cffe0a
Yelitza Parra <yelitza.parra@cakedc.com>
2016-05-16 17:37:03 -0500

    #17605 WIP team page

---
+++ config/team.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+	'Team' => [
+		[
+			'name' => 'Larry Master',
+			'description' => __('Larry Masters the Co-Founder of the CakePHP project, first released in May 2005, and has contributed as a lead developer and advisor ever since. He has been involved in Open Source development for many years, dating back to his first contributions made to The Exchange Project, now called osCommerce. Prior to developing the CakePHP framework and starting Cake Software Foundation, Inc. he was a core developer of PostNuke, now continued as Zikula. He has 30 plus years experience in software development, system administration, and database design. His passion for excellence is shown in the code he writes and time dedicated to the CakePHP framework development over the past 11 years.'),
+			'language' => __('English, US English, Broken English, Klingon'),
+			'twitter' => 'phpnut',
+			'github' => 'phpnut',
+			'mycake' => 'phpnut',
+			'role' => __('Co-Founder')
+		],
+		[
+			'name' => 'Mark Story',
+			'description' => __('Mark is a core member of the CakePHP team, serving as a lead developer since late 2009. He is also a developer at FreshBooks. CakePHP Developer, Dad, MtG nerd, and moonlight Illustrator.'),
+			'language' => __('English'),
+			'twitter' => 'mark_story',
+			'github' => 'markstory',
+			'mycake' => 'markstory',
+			'role' => __('Lead Developer')
+		],
+		[
+			'name' => 'José Lorenzo Rodríguez',
+			'description' => __('Jose is a full-stack software developer that loves to solve difficult problems. He has been contributing to the CakePHP framework since 2009, and he often does talks about its amazing ORM. In his spare time, he teaches and practices mindfulness, ride his bike, or learns new programming languages.'),
+			'language' => __('English, Spanish, French and Danish'),
+			'twitter' => 'jose_zap',
+			'github' => 'lorenzo',
+			'mycake' => 'lorenzo',
+			'role' => __('ORM Developer')
+		]
+	]
+];
---
+++ src/Template/Pages/team.ctp
@@ -0,0 +1,39 @@
+<?php
+use Cake\Core\Configure;
+$title = __('CakePHP Rapid Development PHP Framework | Team');
+$description = __('Team');
+$this->assign('title', $title);
+$this->assign('meta', $this->Html->meta('description', $description));
+$this->assign('socialMeta', $this->Meta->socialTags(['title' => $title, 'description' => $description]));
+$team = Configure::read('Team');
+?>
+
+<section id="team-cake">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12 text-center t-team">
+				<h1><span class="glyph_range">V</span><em>The</em> <strong>Bakers</strong><span class="glyph_range">V</span></h1>
+				<h3>Meet the team behind CakePHP</h3>
+			</div>
+			<div class="col-sm-12">
+				<?php foreach($team as $member):?>
+					<div class="col-sm-6 col-md-4 padd-team">
+						<div class="portfolio-el view view-team text-center">
+							<img alt="project" src="images/team/larry.jpg">
+							<a class="mask" data-target="#team-larry" data-toggle="modal">
+								<div class="t-team-read">
+									<h6>read more</h6>
+								</div>
+							</a>
+						</div>
+						<div class="text-team">
+							<div class="d-team"></div>
+							<h3><?= $member['name'] ?></h3>
+							<h5><?= $member['role'] ?></h5>
+						</div>
+					</div>
+				<?php endforeach;?>
+			</div>
+		</div>
+	</div>
+</section>
