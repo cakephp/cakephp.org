b30db174fa4253d480f509bfe3fb105572cd9a43
Yelitza Parra <yelitza.parra@cakedc.com>
2016-05-17 17:45:56 -0500

    #17605 WIP teams, adding images and team members

--- config/team.php
+++ config/team.php
@@ -9,7 +9,8 @@ return [
 			'twitter' => 'phpnut',
 			'github' => 'phpnut',
 			'mycake' => 'phpnut',
-			'role' => __('Co-Founder')
+			'role' => __('Co-Founder'),
+			'image-url' => 'team/larry_masters.jpg'
 		],
 		[
 			'name' => 'Mark Story',
@@ -18,7 +19,8 @@ return [
 			'twitter' => 'mark_story',
 			'github' => 'markstory',
 			'mycake' => 'markstory',
-			'role' => __('Lead Developer')
+			'role' => __('Lead Developer'),
+			'image-url' => 'team/mark_story.jpg'
 		],
 		[
 			'name' => 'José Lorenzo Rodríguez',
@@ -27,7 +29,113 @@ return [
 			'twitter' => 'jose_zap',
 			'github' => 'lorenzo',
 			'mycake' => 'lorenzo',
-			'role' => __('ORM Developer')
-		]
+			'role' => __('ORM Developer'),
+			'image-url' => 'team/jose_lorenzo.jpg'
+		],
+		[
+			'name' => 'Jose Diaz-Gonzalez',
+			'description' => __('A developer based in New York, New York with an extremely active github account and an interest in Docker and Server Monitoring. I\'ve been doing this since around 2008 and sometimes he blogs.'),
+			'language' => __('English'),
+			'twitter' => 'savant',
+			'github' => 'josegonzalez',
+			'mycake' => '',
+			'role' => __('Core Member'),
+			'image-url' => 'team/savant.jpg'
+		],
+		[
+			'name' => 'Admad',
+			'description' => __('A programmer by day and night, you will generally find him lurking in the deepest darkest corners of the internet. He\'s also involved in maintaining lot of popular CakePHP community plugins.'),
+			'language' => __('English'),
+			'twitter' => '',
+			'github' => 'ADmad',
+			'mycake' => 'ADmad',
+			'role' => __('Core Member'),
+			'image-url' => 'team/admad.jpg'
+		],
+		[
+			'name' => 'Bryan Crowe',
+			'description' => __('Bryan Crowe is an open source developer living in New York, NY. He is involved in the CakePHP framework.'),
+			'language' => __('English'),
+			'twitter' => '_beakman',
+			'github' => 'bcrowe',
+			'mycake' => 'beakman',
+			'role' => __('Core Member'),
+			'image-url' => 'team/bryan_crowe.jpg'
+		],
+		[
+			'name' => 'Florian Kramer',
+			'description' => __('A software developer by profession and not just because it’s a job. Florian is a core CakePHP team
+							developer as well as having worked with CakeDC itself, doing CakePHP development since 2007. Florian sees
+							programming as a true profession and not just a job for him, he likes to work on open source projects after
+							work hours and has an interest in Unity3D and c# as well. He’s working as lead developer for PSA Publishers
+							Ltd'),
+			'language' => __('English, German'),
+			'twitter' => 'burzum793',
+			'github' => 'burzum',
+			'mycake' => 'burzum',
+			'role' => __('Core Member'),
+			'image-url' => 'team/florian_kramer.jpg'
+		],
+		[
+			'name' => 'Mark Scherer',
+			'description' => __('Mark has been a web-software enthusiast and Open Source Contributor for many years.
+					He is a German Software Developer living in Berlin who started to use CakePHP in the 1.2 days.
+					As one of the core developers of CakePHP he is a huge fan of rapid development - and has been using
+					CakePHP a lot for this in his applications so far.
+					On his blog dereuromark.de he sometimes blogs about web and CakePHP related topics.'),
+			'language' => __('English, German'),
+			'twitter' => 'dereuromark',
+			'github' => 'dereuromark',
+			'mycake' => 'dereuromark',
+			'role' => __('Core Member'),
+			'image-url' => 'team/mark_scherer.png'
+		],
+		[
+			'name' => 'Christian Winther (Jippi)',
+			'description' => __('CTO at @bownty, @CakePHP core developer, tech advisor at @getlinkfire - lover of technology
+			 					and servers - author of FriendsOfCake/crud'),
+			'language' => __('English'),
+			'twitter' => 'Jippi',
+			'github' => 'jippi',
+			'mycake' => 'jippi',
+			'role' => __('Core Member'),
+			'image-url' => 'team/christian_winther.jpg'
+		],
+		[
+			'name' => 'Walther Lalk (Dakota)',
+			'description' => __('Walther is a software engineer/developer primarily working in PHP (Using CakePHP as his framework of choice).
+								Also a core member of the team, as well as the Croogo and Friends of Cake development team.
+								When he is not developing or behind his PC screen, you can find him either spending time with his family,
+								or at Scouts, where he is an adult leader/Scouter.'),
+			'language' => __('English'),
+			'twitter' => 'dakotairene',
+			'github' => 'dakota',
+			'mycake' => 'dakota',
+			'role' => __('Core Member'),
+			'image-url' => 'team/walther_lalk.jpg'
+		],
+		/*[
+			'name' => 'Juan Basso (jrbasso)',
+			'description' => __('An architect and engineer that loves software performance. Finds the balance between maintainable code and
+							performance. Director at Zumba incentivize the whole team to contribute in open source projects and participate
+							from meetups and conferences.'),
+			'language' => __('English, Portuguese'),
+			'twitter' => 'jrbasso',
+			'github' => 'jrbasso',
+			'mycake' => 'jrbasso',
+			'role' => __(''),
+			'image-url' => ''
+		], */
+		[
+			'name' => 'Kurita Takashi',
+			'description' => __('Kurita is a PHP programmer living in Tokyo. Currently he works in web application security. Joined CakePHP
+								core team in February 2016 and has been working closely with the team on security fixes.'),
+			'language' => __('Japanese'),
+			'twitter' => 'chinpei215',
+			'github' => 'chinpei215',
+			'mycake' => 'chinpei215',
+			'role' => __('Core Member'),
+			'image-url' => 'team/chinpei215.png'
+		],
 	]
 ];
--- src/Template/Pages/team.ctp
+++ src/Template/Pages/team.ctp
@@ -6,23 +6,26 @@ $this->assign('title', $title);
 $this->assign('meta', $this->Html->meta('description', $description));
 $this->assign('socialMeta', $this->Meta->socialTags(['title' => $title, 'description' => $description]));
 $team = Configure::read('Team');
+$i = 0;
+$elementsByRow = 3;
 ?>
 
 <section id="team-cake">
 	<div class="container">
 		<div class="row">
 			<div class="col-sm-12 text-center t-team">
-				<h1><span class="glyph_range">V</span><em>The</em> <strong>Bakers</strong><span class="glyph_range">V</span></h1>
-				<h3>Meet the team behind CakePHP</h3>
+				<h1><span class="glyph_range">V</span><em>The</em><strong>Bakers</strong><span class="glyph_range">V</span></h1>
+				<h3><?= __('Meet the team behind CakePHP')?></h3>
 			</div>
 			<div class="col-sm-12">
-				<?php foreach($team as $member):?>
+				<?php foreach($team as $member):
+					$i++;?>
 					<div class="col-sm-6 col-md-4 padd-team">
 						<div class="portfolio-el view view-team text-center">
-							<img alt="project" src="images/team/larry.jpg">
+							<?= $this->Html->image($member['image-url'], ['alt' => $member['name']])?>
 							<a class="mask" data-target="#team-larry" data-toggle="modal">
 								<div class="t-team-read">
-									<h6>read more</h6>
+									<h6><?= __('read more')?></h6>
 								</div>
 							</a>
 						</div>
@@ -32,8 +35,15 @@ $team = Configure::read('Team');
 							<h5><?= $member['role'] ?></h5>
 						</div>
 					</div>
+					<?php if ($i % $elementsByRow == 0):?>
+						<div class="clearfix hidden-xs hidden-sm"></div>
+					<?php endif;?>
 				<?php endforeach;?>
 			</div>
 		</div>
 	</div>
 </section>
+
+<?= $this->element('home/share')?>
+
+<?= $this->element('home/newsletter')?>
