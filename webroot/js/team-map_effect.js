2d28965fcf822e920456e3ede45066cf6f6a97b0
Jose Alejandro Alvarez Mendoza <jose.mendoza@cakedc.com>
2016-05-23 15:54:39 -0300

    Improving dynamic map effect

--- webroot/js/team-map_effect.js
+++ webroot/js/team-map_effect.js
@@ -1,22 +1,20 @@
-
-google.charts.load('current', {'packages':['geochart']});
-google.charts.setOnLoadCallback(drawRegionsMap);
-
-function drawRegionsMap() {
-
-    var country = document.getElementsByClassName('country');
-    console.log(country);
-
-    var data = google.visualization.arrayToDataTable([
-        ['Country'],
-        [country]
-    ]);
-
-    var options = {
-        defaultColor: '#D33C44'
-    };
-
-    var chart = new google.visualization.GeoChart(document.getElementById(country));
-
-    chart.draw(data, options);
-}
+(function () {
+    google.charts.load('current', {'packages':['geochart']});
+    google.charts.setOnLoadCallback(drawRegionsMap);
+    function drawRegionsMap(teamMember) {
+        var country = $('#' + teamMember + '-country').val();
+        var data = google.visualization.arrayToDataTable([
+            ['Country'],
+            [country]
+        ]);
+        var options = {
+            defaultColor: '#D33C44'
+        };
+        var chart = new google.visualization.GeoChart(document.getElementById(teamMember + '-map'));
+        chart.draw(data, options);
+    }
+    $("a").click(function() {
+        var teamMember = $(this).attr("data-target").split('#');
+        drawRegionsMap(teamMember[1]);
+    });
+})();
