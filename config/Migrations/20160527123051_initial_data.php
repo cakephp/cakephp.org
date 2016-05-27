8c5a075d9a988d0faf1e71df0a7a7727f263410e
Yelitza Parra <yelitza.parra@cakedc.com>
2016-05-27 14:44:32 +0200

    Adding projects to showcase pages

---
+++ config/Migrations/20160527123051_initial_data.php
@@ -0,0 +1,34 @@
+<?php
+use Phinx\Migration\AbstractMigration;
+use Cake\Datasource\ConnectionManager;
+
+class InitialData extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function up()
+    {
+		$connection = ConnectionManager::get('default');
+		$sql = file_get_contents(CONFIG . 'sql' . DS . 'initial_data.sql');
+		$connection->execute($sql);
+    }
+}
