684208d851db875d3824659a768bf0954cdcf87a
Marc Würth <ravage@bluewin.ch>
2016-06-06 18:09:26 +0200

    IRC does not need to be translated

--- config/site.php
+++ config/site.php
@@ -123,7 +123,7 @@ return [
 					'irc' => [
 						'url' => 'https://kiwiirc.com/client/irc.freenode.net#cakephp',
 						'options' => ['target' => '_blank'],
-						'title' => __('IRC')
+						'title' => 'IRC'
 					],
 					'slack' => [
 						'url' => 'http://cakesf.herokuapp.com/',
