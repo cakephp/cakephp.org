7b33bfc18b309b63d616fab7d451bc947356a0b9
ADmad <ADmad@users.noreply.github.com>
2017-12-06 18:18:03 +0530

    Remove default serialization of all view vars.

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -89,11 +89,6 @@ class AppController extends Controller
      */
     public function beforeRender(Event $event)
     {
-        if (!array_key_exists('_serialize', $this->viewVars) &&
-            in_array($this->response->type(), ['application/json', 'application/xml'])
-        ) {
-            $this->set('_serialize', true);
-        }
         $this->set([
             '_version' => Configure::read('App.version'),
         ]);
