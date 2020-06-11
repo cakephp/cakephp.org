73e6d18b2c05b8cca16e70b072439d4b9d5455d2
Jorge González <steinkel@gmail.com>
2020-06-11 12:56:18 +0100

    fix permissions

--- config/permissions.php
+++ config/permissions.php
@@ -48,85 +48,42 @@
         }
     ],
  */
+$baseUsersPermissions = include ROOT . '/vendor/cakedc/users/config/permissions.php';
 
-return [
+$appPermissions = [
     'CakeDC/Auth.permissions' => [
-        //all bypass
-        [
-            'prefix' => false,
-            'plugin' => 'CakeDC/Users',
-            'controller' => 'Users',
-            'action' => [
-                // LoginTrait
-                'socialLogin',
-                'login',
-                'logout',
-                'socialEmail',
-                'verify',
-                // RegisterTrait
-                'register',
-                'validateEmail',
-                // PasswordManagementTrait used in RegisterTrait
-                'changePassword',
-                'resetPassword',
-                'requestResetPassword',
-                // UserValidationTrait used in PasswordManagementTrait
-                'resendTokenValidation',
-                'linkSocial',
-                //U2F actions
-                'u2f',
-                'u2fRegister',
-                'u2fRegisterFinish',
-                'u2fAuthenticate',
-                'u2fAuthenticateFinish',
-            ],
-            'bypassAuth' => true,
-        ],
-        [
-            'prefix' => false,
-            'plugin' => 'CakeDC/Users',
-            'controller' => 'SocialAccounts',
-            'action' => [
-                'validateAccount',
-                'resendValidation',
-            ],
-            'bypassAuth' => true,
-        ],
-        //admin role allowed to all the things
-        [
-            'role' => 'admin',
-            'prefix' => '*',
-            'extension' => '*',
-            'plugin' => '*',
-            'controller' => '*',
-            'action' => '*',
-        ],
-        //specific actions allowed for the all roles in Users plugin
-        [
-            'role' => '*',
-            'plugin' => 'CakeDC/Users',
-            'controller' => 'Users',
-            'action' => ['profile', 'logout', 'linkSocial', 'callbackLinkSocial'],
-        ],
-        [
-            'role' => '*',
-            'plugin' => 'CakeDC/Users',
-            'controller' => 'Users',
-            'action' => 'resetOneTimePasswordAuthenticator',
-            'allowed' => function (array $user, $role, \Cake\Http\ServerRequest $request) {
-                $userId = \Cake\Utility\Hash::get($request->getAttribute('params'), 'pass.0');
-                if (!empty($userId) && !empty($user)) {
-                    return $userId === $user['id'];
-                }
-
-                return false;
-            },
-        ],
-        //all roles allowed to Pages/display
         [
             'role' => '*',
             'controller' => 'Pages',
             'action' => 'display',
+            'bypassAuth' => true,
         ],
-    ],
+        [
+            'role' => '*',
+            'controller' => 'Changelogs',
+            'action' => ['index', 'view'],
+            'bypassAuth' => true,
+        ],
+        [
+            'role' => ['user'],
+            'controller' => ['Dashboards'],
+            'action' => ['index'],
+            'prefix' => 'admin',
+        ],
+        [
+            'role' => ['user'],
+            'controller' => ['Projects'],
+            'action' => '*',
+            'prefix' => 'admin',
+        ],
+        [
+            'role' => '*',
+            'plugin' => 'DebugKit',
+            'controller' => '*',
+            'action' => '*',
+            'bypassAuth' => true,
+        ],
+    ]
 ];
+
+return $appPermissions + $baseUsersPermissions;
