b326aed252591abbc3ff57bee77e5f5deaf3d14b
Arhell <arhell333@gmail.com>
2021-12-31 01:14:04 +0200

    Accessibility - add titles to video (js)

--- webroot/js/youtube_videos.js
+++ webroot/js/youtube_videos.js
@@ -11,7 +11,7 @@
 			videoHtml = '<div class="col-md-4 col-sm-6 col-xs-12 el news_ t-videos">'
 			+ '<div class="">'
             	+ '<div class="embed-responsive embed-responsive-16by9">'
-					+ '<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/'+videoId+'" frameborder="0" allowfullscreen></iframe>'
+					+ '<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/'+videoId+'" frameborder="0" allowfullscreen title="video"></iframe>'
 				+ '</div>'
 				+ '<h4><a title="'+title+'" href="https://www.youtube.com/watch?v='+videoId+'" target="_blank">'+title+'</a></h4>'
 			+ '</div>'
